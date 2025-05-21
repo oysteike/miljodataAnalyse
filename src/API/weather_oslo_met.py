04c46e99954f18eac243e68a38ebde6e4e060c18
Georg B. Rommetveit <georgbro@stud.ntnu.no>
2025-05-21 14:07:56 +0200

    Henter noen forskjellige værformer, slik at vi kan gjøre beregninger.

--- src/API/weather_oslo_met.py
+++ src/API/weather_oslo_met.py
@@ -93,15 +93,21 @@ if __name__ == "__main__":
     ref_time = "2015-01-01/2025-01-01"
     source_id = "SN90450"
 
-    """
-    fetch1 = FrostDataFetcher(client_id, source_id, 'sum(precipitation_amount P1D)',  '2015-01-01/2025-01-01', "Precipitation_data.csv")
-    fetch2 = FrostDataFetcher(client_id, source_id, 'sum(duration_of_sunshine P1D)', '2015-01-01/2025-01-01', "Sunshine_data.csv")
-    fetch3 = FrostDataFetcher(client_id, source_id, 'max(surface_air_pressure P1D)', '2015-01-01/2025-01-01', "Pressure_data.csv")
+    
+    fetch1 = FrostDataFetcher(client_id, source_id, 'sum(precipitation_amount P1D)',  '2015-01-01/2025-01-01', "oslo_since_2015-2025/Precipitation.csv")
+    fetch2 = FrostDataFetcher(client_id, source_id, 'sum(duration_of_sunshine P1D)', '2015-01-01/2025-01-01', "oslo_since_2015-2025/Sunshine.csv")
+    fetch3 = FrostDataFetcher(client_id, source_id, 'max(surface_air_pressure P1D)', '2015-01-01/2025-01-01', "oslo_since_2015-2025/Pressure.csv")
+    fetch4 = FrostDataFetcher(client_id, source_id, 'mean(air_temperature P1D)',  '2015-01-01/2025-01-01', "oslo_since_2015-2025/Temperature.csv")
+    fetch5 = FrostDataFetcher(client_id, source_id, 'mean(wind_speed P1D)',  '2015-01-01/2025-01-01', "oslo_since_2015-2025/Wind.csv")
+    fetch6 = FrostDataFetcher(client_id, source_id, 'mean(relative_humidity P1D)',  '2015-01-01/2025-01-01', "oslo_since_2015-2025/Humidity.csv")
     fetch1.run()
     fetch2.run()
     fetch3.run()
+    fetch4.run()
+    fetch5.run()
+    fetch6.run()    
+
     """
-    
     for station_name in ['Agder', 'Innlandet', 'Oslo', 'Viken', 'Vestfold og Telemark', 'Møre og Romsdal', 'Nordland', 'Vestland', 'Trøndelag', 'Troms og Finnmark', 'Rogaland']:
         
         # Hent alle lokasjoner i regionen
@@ -134,3 +140,4 @@ if __name__ == "__main__":
                     stationsdata_path=stationsdata_path # Sti til stationsdata.csv
                 )
                 fetch.run()
+    """
